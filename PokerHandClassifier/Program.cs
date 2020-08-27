040ed83edb110cbad9aef32cd5d4f36bc8d1a99b
PlotzBot2000 <jphilip.lotz@gmail.com>
2020-08-27 16:37:46 +0200

    Card verification added

--- PokerHandClassifier/Program.cs
+++ PokerHandClassifier/Program.cs
@@ -17,26 +17,29 @@ namespace PokerHandClassifier
                     //Launch and obtain Cards
                     Console.Write("Please enter your cards: ");
                     var pokerHand = Console.ReadLine();
+                    //string pokerHand = "AS, 10C, 10C, 3D, 3S";   //TODO: remove --for debugging
+
 
                     //Check is cards are valid
                     var checkCards = new CardsChecker();
                     string validCardsResponse = checkCards.VerifyCards(pokerHand);
-                    
+
                     if (validCardsResponse != "")
                     {
                         Console.WriteLine("The poker hand is invalid. " + validCardsResponse);
-                        Console.WriteLine("e.g. AS, KD, 10C, 9H, 9C");
+                        Console.WriteLine("Please enter a correct list of cards. e.g. AS, KD, 10C, 9H, 9C");
                     }
+                    else
+                    {
+                        //Call Card classifier
+                        var API = new APIUtil();
+                        string handType = API.MakePostRequest(); //TODO: Complete
 
-                    //Call Card classifier
-                    var API = new APIUtil();
-                    string handType = API.MakePostRequest(); //TODO: Complete
-
-                    Console.WriteLine("Your best hand is " + handType);
-                    Console.WriteLine("Press any key to continue");
-                    var temp = Console.ReadLine();
-                    Console.Clear();
-
+                        Console.WriteLine("Your best hand is " + handType);
+                        Console.WriteLine("Press any key to continue");
+                        var temp = Console.ReadLine();
+                        Console.Clear();
+                    }
 
                     //Check for retry
                     Console.Write("Do you want to check another poker hand? (Y/N): ");
--- PokerTests/UnitTest1.cs
+++ PokerTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using PokerHandClassifier;
 
 namespace PokerTests
 {
@@ -7,14 +8,159 @@ namespace PokerTests
         [SetUp]
         public void Setup()
         {
+
         }
 
-        //TODO: Add tests for API Calls, 
+        [Test]
+        public void AmountTest1()
+        {
+            var checkCards = new CardsChecker();
+            string testCards = "AS, 10C, 10C, 3D, 3S";
+            string[] testCardArray = testCards.ToUpper().Split(',');
+
+            try
+            {
+                checkCards.VerifyAmount(testCardArray);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
 
         [Test]
-        public void Test1()
+        public void AmountTest2()
         {
-            Assert.Pass();
+            var checkCards = new CardsChecker();
+            string testCards = "AS, 10C, 3D, 3S";
+            string[] testCardArray = testCards.ToUpper().Split(',');
+
+            try
+            {
+                checkCards.VerifyAmount(testCardArray);
+            }
+            catch
+            {
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void SuitTest1()
+        {
+            var checkCards = new CardsChecker();
+            string testCards = "AS, 10C, 10C, 3D, 3S";
+            string[] testCardArray = testCards.ToUpper().Split(',');
+
+            try
+            {
+                checkCards.VerifySuit(testCardArray);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void SuitTest2()
+        {
+            var checkCards = new CardsChecker();
+            string testCards = "AS, 10C, 10C, 3X, 3S";
+            string[] testCardArray = testCards.ToUpper().Split(',');
+
+            try
+            {
+                checkCards.VerifySuit(testCardArray);
+            }
+            catch
+            {
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void ValueTest1()
+        {
+            var checkCards = new CardsChecker();
+            string testCards = "AS, 10C, 10C, KD, 3S";
+            string[] testCardArray = testCards.ToUpper().Split(',');
+
+            try
+            {
+                checkCards.VerifySuit(testCardArray);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void ValueTest2()
+        {
+            var checkCards = new CardsChecker();
+            string testCards = "AS, 12C, 10C, 3X, 3S";
+            string[] testCardArray = testCards.ToUpper().Split(',');
+
+            try
+            {
+                checkCards.VerifySuit(testCardArray);
+            }
+            catch
+            {
+                Assert.Pass();
+            }
+        }
+        [Test]
+        public void ValueTest3()
+        {
+            var checkCards = new CardsChecker();
+            string testCards = "AS, 10C, BC, 3X, 3S";
+            string[] testCardArray = testCards.ToUpper().Split(',');
+
+            try
+            {
+                checkCards.VerifySuit(testCardArray);
+            }
+            catch
+            {
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void DuplicateTest1()
+        {
+            var checkCards = new CardsChecker();
+            string testCards = "AS, 10C, 10C, KD, 3S";
+            string[] testCardArray = testCards.ToUpper().Split(',');
+
+            try
+            {
+                checkCards.VerifySuit(testCardArray);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void DuplicateTest2()
+        {
+            var checkCards = new CardsChecker();
+            string testCards = "AS, AS, 10C, KD, 3S";
+            string[] testCardArray = testCards.ToUpper().Split(',');
+
+            try
+            {
+                checkCards.VerifySuit(testCardArray);
+            }
+            catch
+            {
+                Assert.Pass();
+            }
         }
     }
 }
